ec19d914be852f357fe3e80ac06735be545fffd0
Dee <twitchg650@gmail.com>
2023-05-12 12:23:25 -0500

    Codewars for Dec

---
+++ shortcut.js
@@ -0,0 +1,14 @@
+/**
+ * Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+Examples
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"
+ */
+
+function shortcut(string) {
+    // Use regular expression to remove lowercase vowels
+    return string.replace(/[aeiou]/g, '');
+  }
