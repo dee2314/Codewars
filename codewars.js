ea6d114e8cf5777fb53f13021b09d90027fd20e2
Dee <twitchg650@gmail.com>
2023-08-02 19:29:16 -0500

    codewars problems

--- codewars.js
+++ codewars.js
@@ -31,4 +31,19 @@ function addLength(str) {
   //start-here
    return str.split(" ").map(s => `${s} ${s.length}`)
     };
-  
+
+/*Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+    
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+    
+Example(Input --> Output)*/
+
+function findNeedle(haystack) {
+  // your code here
+  const needleIndex = haystack.indexOf('needle');
+  return `found the needle at position ${needleIndex}`
+}
