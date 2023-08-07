4cbdb4fbba3fd8799fdf91622c2a3fd923b88c24
Dee <twitchg650@gmail.com>
2023-08-06 20:00:49 -0500

    CodeWars Solution for 8/6/23

--- codewars.js
+++ codewars.js
@@ -100,6 +100,42 @@ function sumTwoSmallestNumbers(numbers) {
   let second = Math.min(...numbers)
   return first + second
 }
-/** */
-/** */
+/** Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+Example
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+
+Don't forget the space after the closing parentheses!
+
+*/
+//SOLUTION
+function createPhoneNumber(numbers){
+  var format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
+
+/**Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8 */
+//SOLUTION
+function rowSumOddNumbers(n) {
+	// TODO
+  return n * n * n;
+}
 /** */
