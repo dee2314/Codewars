12530beb9ce9cb0d719bac0ed92028a3145fbb4f
Dee <twitchg650@gmail.com>
2023-08-08 12:07:10 -0500

    Add Coding Challenge for 8-8-23

--- codewars.js
+++ codewars.js
@@ -67,3 +67,14 @@ function boolToWord( bool ){
   //...
    return bool ? 'Yes':'No';
 }
+
+/*Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow' */
+
+//Solution
+function solution(str){
+  return str.split('').reverse().join('');  
+}
+
