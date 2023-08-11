1aa3ca2d921029f644317c037d7895b037ebee67
Dee <twitchg650@gmail.com>
2023-08-10 19:44:10 -0500

    Codewars for August 10th

--- codewars.js
+++ codewars.js
@@ -138,4 +138,41 @@ function rowSumOddNumbers(n) {
 	// TODO
   return n * n * n;
 }
+/** Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+Examples
+Input: [1, 5.2, 4, 0, -1]
+Output: 9.2
+
+Input: []
+Output: 0
+
+Input: [-2.398]
+Output: -2.398
+
+Assumptions
+You can assume that you are only given numbers.
+You cannot assume the size of the array.
+You can assume that you do get an array and if the array is empty, return 0.
+What We're Testing
+We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
+Advanced users may find this extremely easy and can easily write this in one line.*/
+//SOLUTION
+function sum(numbers) {
+  return numbers.reduce((a, b) => a + b, 0);
+}
+/**Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+
+ */
+//SOLUTION
+function removeEveryOther(arr){
+  //your code here
+  return arr.filter((element, index) => index % 2 === 0);
+}
 /** */
+//SOLUTION
