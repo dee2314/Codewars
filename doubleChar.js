df72653d37332fb3ec282ed7821f831d0522753b
Dee <twitchg650@gmail.com>
2023-08-28 16:20:01 -0500

    Codewars For August 28th

---
+++ doubleChar.js
@@ -0,0 +1,20 @@
+/*Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "*/
+
+
+//SOLUTION
+function doubleChar(str) {
+    // Your code here
+    let result = '';
+    
+    for (let i = 0; i < str.length; i++){
+      result += str.charAt(i) + str.charAt(i);
+    }
+    
+    return result
+  }
+  
