3744e02b6aae6d23846afdc4b7866f1094331b41
Dee <twitchg650@gmail.com>
2023-08-29 15:15:33 -0500

    Update

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,3 +1,5 @@
+//Rock Paper Scissors
+//Let's play! You have to return which player won! In case of a draw return Draw!.
 const rps = (p1, p2) => {
     if (p1 === p2) return "Draw!";
     var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
