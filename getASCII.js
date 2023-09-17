eb1690267eb1988890402e760214e133ff5b6cee
Dee <twitchg650@gmail.com>
2023-09-17 18:35:12 -0500

    CodeWars Challenge

---
+++ getASCII.js
@@ -0,0 +1,9 @@
+/**Get ASCII value of a character.
+
+For the ASCII table you can refer to http://www.asciitable.com/ */
+
+//SOLUTION
+function getASCII(c){
+    const asciiValue = c.charCodeAt(0);
+  return asciiValue;
+}
