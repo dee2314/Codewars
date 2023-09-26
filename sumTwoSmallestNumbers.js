1694197316a6da2ee5ce21c2aa2031189d8343c6
Dee <twitchg650@gmail.com>
2023-09-26 08:36:10 -0500

    Adding Breakdown to a CodeWars Problem;

--- sumTwoSmallestNumbers.js
+++ sumTwoSmallestNumbers.js
@@ -11,4 +11,18 @@ function sumTwoSmallestNumbers(numbers) {
     return first + second
   }
 
- 
+ //BREAKDOWN
+ /**This function, sumTwoSmallestNumbers, takes an array of numbers as its input and 
+  * is designed to find and sum the two smallest numbers in that array.
+
+1. We begin by finding the smallest number in the input array numbers using the Math.min(...numbers) function. 
+This gives us the smallest value in the array, and we store it in the variable first.
+
+2. Next, we locate the index of the first smallest number in the numbers array using numbers.indexOf(first). 
+We then remove it from the array using splice. This ensures that the smallest number is no longer part of the array, so we can find the second smallest number.
+
+3. After removing the first smallest number, we again use Math.min(...numbers) to find the smallest number in the modified numbers array,
+ and we store it in the variable second.
+
+4. Finally, we return the sum of first and second, 
+which are the two smallest numbers in the original input array. */
