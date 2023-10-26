5015fc7483bef5a0114979c00f0ffc6e4b7be801
Dee <twitchg650@gmail.com>
2023-10-26 17:59:48 -0500

    CodeWars October

---
+++ findAverage.js
@@ -0,0 +1,20 @@
+/**
+ * Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+
+
+ */
+
+//SOLUTION
+function findAverage(array) {
+    if (array.length === 0) {
+      return 0;
+    } else {
+      const sum = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  
+      const average = sum / array.length;
+  
+      return average;
+    }
+  }
