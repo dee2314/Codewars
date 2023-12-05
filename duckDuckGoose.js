0c0a175f7c775e17e151b01e5c162ec56c69e412
Dee <twitchg650@gmail.com>
2023-12-05 11:28:05 -0600

    Daily Codewars

---
+++ duckDuckGoose.js
@@ -0,0 +1,15 @@
+/**
+ * The objective of Duck, duck, goose is to walk in a circle, tapping on each player's head until one is chosen.
+
+Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based),
+ return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
+*/
+
+//SOLUTION
+function duckDuckGoose(players, goose) {
+    // Ensure goose index is within the bounds of the array
+    const gooseIndex = (goose - 1) % players.length;
+  
+    // Return the name of the chosen Player
+    return players[gooseIndex].name;
+  }
