5bed3954837e0997df483f9fcb38743d9f587bd4
Dee <twitchg650@gmail.com>
2023-12-23 10:17:50 -0600

    codewars for dec

---
+++ enough.js
@@ -0,0 +1,25 @@
+/**
+ * The Story:
+Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
+
+Task Overview:
+You have to write a function that accepts three parameters:
+
+cap is the amount of people the bus can hold excluding the driver.
+on is the number of people on the bus excluding the driver.
+wait is the number of people waiting to get on to the bus excluding the driver.
+If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+Usage Examples:
+ */
+
+//SOLUTION
+function enough(cap, on, wait) {
+    const availableSpace = cap - on;
+  
+    if (availableSpace >= wait) {
+      return 0; // There is enough space for all waiting passengers
+    } else {
+      return wait - availableSpace; // Return the number of passengers who can't take the bus
+    }
+  }
