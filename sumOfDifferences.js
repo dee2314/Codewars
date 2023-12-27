a48587863206b363aeb55f236ac4d188545dd373
Dee <twitchg650@gmail.com>
2023-12-27 10:28:50 -0600

    codewars for dec

---
+++ sumOfDifferences.js
@@ -0,0 +1,19 @@
+/**
+Your task is to sum the differences between consecutive pairs in the array in descending order.
+ */
+
+  //SOLUTION
+  function sumOfDifferences(arr) {
+    // Sort the array in descending order
+    const sortedArr = arr.sort((a, b) => b - a);
+  
+    // Initialize the sum variable
+    let sum = 0;
+  
+    // Iterate through the array and sum the differences between consecutive pairs
+    for (let i = 0; i < sortedArr.length - 1; i++) {
+      sum += sortedArr[i] - sortedArr[i + 1];
+    }
+  
+    return sum;
+  }
