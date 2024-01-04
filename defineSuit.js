8a1b1ab67a6302d91f96220b4bdf2afbd50e55b9
Dee <twitchg650@gmail.com>
2024-01-04 16:50:25 -0600

    Codewars for Jan

---
+++ defineSuit.js
@@ -0,0 +1,28 @@
+/**
+ * You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+Our deck (is preloaded):
+
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+ */
+
+//SOLUTION
+function defineSuit(card) {
+    const suit = card.charAt(card.length - 1);
+  
+    switch (suit) {
+      case '♣':
+        return 'clubs';
+      case '♦':
+        return 'diamonds';
+      case '♥':
+        return 'hearts';
+      case '♠':
+        return 'spades';
+      default:
+        return 'unknown'; // Handle unexpected input gracefully
+    }
+  }
