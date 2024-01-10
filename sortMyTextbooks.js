467c434b7a0d47a6688b60f7060b35a47493f915
Dee <twitchg650@gmail.com>
2024-01-10 17:07:03 -0600

    Codewars for Jan

---
+++ sortMyTextbooks.js
@@ -0,0 +1,14 @@
+/**
+ * HELP! Jason can't find his textbook! It is two days before the test date, and Jason's textbooks are all out of order! Help him sort a list (ArrayList in java) full of textbooks by subject, so he can study before the test.
+
+The sorting should NOT be case sensitive
+ */
+
+//SOLUTION
+function sorter(textbooks) {
+    return [...textbooks].sort((a, b) => {
+      a = a.toLowerCase();
+      b = b.toLowerCase();
+      return (a > b) - (a < b);
+    });
+  }
