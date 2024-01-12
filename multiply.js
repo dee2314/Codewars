fa52741670e84135d70a74b8b966ce48dbbb916b
Dee <twitchg650@gmail.com>
2024-01-12 07:25:55 -0600

    Codewars Daily

--- multiply.js
+++ multiply.js
@@ -1,7 +1,7 @@
 /**This code does not execute properly. Try to figure out why. */
 /* ORIGINAL CODE function multiply(a, b){
 a * b*/
-//SOLUTION 
+//SOLUTION FOR THE PROBLEM
 function multiply (a,b){
     return a * b
     };
