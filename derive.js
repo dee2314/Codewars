ac312e698db479749dcdf3968e4cd4328acbb784
Dee <twitchg650@gmail.com>
2024-01-25 18:33:15 -0600

    Codewars for Jan

---
+++ derive.js
@@ -0,0 +1,19 @@
+/**
+ * This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent.
+
+Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to return an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
+ */
+
+//SOLUTION
+function derive(coefficient, exponent) {
+    const newCoefficient = coefficient * exponent;
+    const newExponent = exponent - 1;
+  
+    if (newExponent === 0) {
+      return newCoefficient.toString();
+    } else if (newExponent === 1) {
+      return `${newCoefficient}x`;
+    } else {
+      return `${newCoefficient}x^${newExponent}`;
+    }
+  }
