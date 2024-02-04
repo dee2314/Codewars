d314013362933de8c13e63fc57e5b11b3074fd3f
Dee <twitchg650@gmail.com>
2024-02-04 06:59:05 -0600

    Codewars Daily

---
+++ breakCamelCase.js
@@ -0,0 +1,13 @@
+/**
+ * Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+ */
+
+//SOLUTION
+function solution(string) {
+    return string.replace(/([A-Z])/g, " $1");
+  }
