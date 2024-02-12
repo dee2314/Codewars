becb230bf77a2d7a23807b83cd041fa258920572
Dee <twitchg650@gmail.com>
2024-02-12 07:55:02 -0600

    Daily

--- neutralise.js
+++ neutralise.js
@@ -3,7 +3,7 @@ Given two strings comprised of + and -, return a new string which shows how the 
 
 When positives and positives interact, they remain positive.
 When negatives and negatives interact, they remain negative.
-But when negatives and positives interact, they become neutral, and are shown as the number 0.
+When negatives and positives interact, they become neutral, and are shown as the number 0.
 */
 
 //SOLUTION
