082a60fdb3ce0b91fe63bb4c367e0fd9985c12b9
Dee <twitchg650@gmail.com>
2024-02-23 08:50:05 -0600

    Codewars for Feb

---
+++ classCircleclassPoint.js
@@ -0,0 +1,33 @@
+/**
+ * This series of katas will introduce you to basics of doing geometry with computers.
+
+Write the function circleArea/CircleArea which takes in a Circle object and calculates the area of that circle.
+The Circle class can be seen below:
+
+// Represents a Circle where center is a Point and radius is a Number
+class Circle {
+  constructor(center, radius) { 
+    this.center = center; 
+    this.radius = radius;
+  }
+}
+And the Point class can be seen below:
+
+// Represents a Point where x and y are Numbers
+class Point {
+  constructor (x,y) { 
+    this.x = x;
+    this.y = y; 
+  }
+}
+ */
+
+//SOLUTION
+function circleArea(circle) {
+    // Assuming circle has a center with x and y attributes, and a radius.
+    const radius = circle.radius;
+    const area = Math.PI * Math.pow(radius, 2);
+    
+    // Round the answer to 6 decimal places.
+    return Math.round(area * 1e6) / 1e6;
+  }
