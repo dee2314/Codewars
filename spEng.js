ff9912edbf3d7f876f7d315abb7825d952e09fb4
Dee <twitchg650@gmail.com>
2024-02-25 08:54:51 -0600

    Codewars for Feb

---
+++ spEng.js
@@ -0,0 +1,17 @@
+/**
+ * Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+Return value as boolean values, true for the string to contains "English", false for it does not.
+ */
+
+//SOLUTION
+function spEng(sentence) {
+    const lowerCaseSentence = sentence.toLowerCase();
+    const lowerCaseEnglish = "english";
+  
+    return lowerCaseSentence.includes(lowerCaseEnglish);
+  }
