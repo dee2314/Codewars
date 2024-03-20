8e8b1b0c7540090a509e77e1857f445c9796360e
Dee <twitchg650@gmail.com>
2024-03-20 09:58:30 -0500

    Codewars Daily

---
+++ ensureQuestions.js
@@ -0,0 +1,17 @@
+/**
+ * Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+
+For example (Input --> Output)
+
+"Yes" --> "Yes?" 
+"No?" --> "No?"
+ */
+
+//SOLUTION
+function ensureQuestion(s) {
+    if (s.endsWith("?")) {
+      return s;
+    } else {
+      return s + "?";
+    }
+  }
