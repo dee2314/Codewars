d806f31a96e944f1c4915db24920f167df2638d7
Dee <twitchg650@gmail.com>
2024-08-02 07:15:44 -0500

    Codewars Daily

--- rainAmount.js
+++ rainAmount.js
@@ -9,5 +9,5 @@ Your task is to debug the code before your plants die!
 function rainAmount(mm){
     if (mm < 40)
       return "You need to give your plant " + (40 - mm) + "mm of water"
-    return "Your plant has had more than enough water for today~" 
+    return "Your plant has had more than enough water for today!" 
   }
