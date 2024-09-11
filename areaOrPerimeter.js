3fd5eb25a56a15a3efc1c461043e7362846e0549
Dee <twitchg650@gmail.com>
2024-09-11 06:42:20 -0500

    Codewars Daily

--- areaOrPerimeter.js
+++ areaOrPerimeter.js
@@ -5,7 +5,7 @@ If it is a square, return its area. If it is a rectangle, return its perimeter.
 Example(Input1, Input2 --> Output):
  */
 
-//SOLUTION FOR THE PROBLEM!
+//SOLUTION FOR THE PROBLEM TO THE CODE
 const areaOrPerimeter = function(length, width) {
     if (length === width) {
       // It's a square, return its area
