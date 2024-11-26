626c120fcc1f665f647d7f3c46424a66ef1bc87b
Dee <twitchg650@gmail.com>
2024-11-26 07:05:45 -0600

    Codewars Daily

--- makeUpperCase.js
+++ makeUpperCase.js
@@ -1,6 +1,7 @@
 //Write a function which converts the input string to uppercase.
 
-//SOLUTION
+//SOLUTION for the problem
+
 function makeUpperCase(str) {
     // Code here
    return str.toUpperCase()
