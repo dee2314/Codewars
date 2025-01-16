aece3fcd84a6a555e3b2f01bd65633c6e6d9d4e9
Dee <twitchg650@gmail.com>
2025-01-16 06:50:04 -0600

    Codewars Daily

---
+++ hexToDex.js
@@ -0,0 +1,10 @@
+/**
+ * Complete the function which converts hex number (given as a string) to a decimal number.
+
+
+ */
+
+//SOLUTION
+function hexToDec(hexString) {
+    return parseInt(hexString, 16);
+  }
