6f32b53f19df04836c23034eafe9d3e4ec1e7559
Dee <twitchg650@gmail.com>
2025-02-15 08:51:53 -0600

    Codewars Daily

--- checkA.js
+++ checkA.js
@@ -7,7 +7,7 @@ Array can contain numbers or strings. X can be either.
 Return true if the array contains the value, false if not.
 ***/
 
-//SOLUTION
+//SOLUTION FOR THE PROBLEM
 function check (a, x){
   return a.includes(x)
 };
