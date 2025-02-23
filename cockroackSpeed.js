407129623519d579dd161c2a3c5bb8a85ba3d5e9
Dee <twitchg650@gmail.com>
2025-02-23 06:59:44 -0600

    Codewars Daily

--- cockroackSpeed.js
+++ cockroackSpeed.js
@@ -3,7 +3,7 @@
  * rounded down to the integer (= floored).*/
 
 
-//SOLUTION FOR THE PROBLEM
+//SOLUTION FOR THE PROBLEM TODAY
 function cockroachSpeed(s) {
     //Good Luck!
     const speedInCmPerSec = Math.floor(s * 100000 / 3600);
