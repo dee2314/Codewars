cc1c8794a410c9ba46e05010682d5c351a93c011
Dee <twitchg650@gmail.com>
2025-03-13 07:37:59 -0500

    Codewars Daily

--- addLength.js
+++ addLength.js
@@ -8,7 +8,7 @@ Your task is to write a function that takes a String and returns an Array/list w
 
 Note: String will have at least one element; words will always be separated by a space. */
 
-//SOLUTION FOR THE PROBLEM
+//SOLUTION FOR THE PROBLEM TODAY
 function addLength(str) {
     //start-here
      return str.split(" ").map(s => `${s} ${s.length}`)
