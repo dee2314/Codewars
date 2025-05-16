b0d1a9f4bd86eff5f5536f9730d2c70be44b7e98
Dee <twitchg650@gmail.com>
2025-05-16 07:03:39 -0500

    Codewars daily

--- mxdiflg.js
+++ mxdiflg.js
@@ -14,7 +14,7 @@ input : 2 strings with substrings separated by ,
 output: number as a string
  */
 
-//SOLUTION
+//SOLUTION FOR THE PROBLEM
 function mxdiflg(a1, a2) {
     if (a1.length === 0 || a2.length === 0) return -1;
 
