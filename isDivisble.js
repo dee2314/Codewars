0b5560cc70ee8ca0e5834301282bf6e00187a5dd
Dee <twitchg650@gmail.com>
2025-05-18 07:02:15 -0500

    Codewars Daily

--- isDivisble.js
+++ isDivisble.js
@@ -2,7 +2,7 @@
 Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
 */
 
-//SOLUTION
+//SOLUTION FOR THE PROBLEM
 function isDivisible(n, x, y) {
   if (n % x === 0 && n % y === 0) {
     return true;
