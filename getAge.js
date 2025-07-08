07fe37ebcdec5ef60e1524e06f5b8a084d382016
Dee <twitchg650@gmail.com>
2025-07-08 18:04:13 -0500

    Codewars Daily

--- getAge.js
+++ getAge.js
@@ -6,7 +6,8 @@ Write a program that returns the girl's age (0-9) as an integer.
 Assume the test input string is always a valid string. 
 For example, the test input may be "1 year old" or "5 years old". 
 The first character in the string is always a number. */
-//SOLUTION
+
+//SOLUTION FOR THE PROBLEM
 function getAge(inputString){
     return parseInt(inputString);
     }
