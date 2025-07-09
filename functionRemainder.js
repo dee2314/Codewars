64032328feb7eb9e9e14d14efbac7e4945d8fed3
Dee <twitchg650@gmail.com>
2025-07-09 18:04:59 -0500

    Codewars Daily

--- functionRemainder.js
+++ functionRemainder.js
@@ -5,7 +5,7 @@ Write a function that accepts two integers and returns the remainder of dividing
 Division by zero should return NaN.
  */
 
-//SOLUTION
+//SOLUTION FOR THE PROBLEM
 function remainder(a, b){
     // Divide the larger argument by the smaller argument and return the remainder
     var min = Math.min(a,b);
