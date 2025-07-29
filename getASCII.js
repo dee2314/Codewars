265e14f79d74d8f746f710d48bac0bca37897229
Dee <twitchg650@gmail.com>
2025-07-29 09:28:32 -0500

    Codewards Dialy

--- getASCII.js
+++ getASCII.js
@@ -2,7 +2,7 @@
 
 For the ASCII table you can refer to http://www.asciitable.com/ */
 
-//SOLUTION
+//SOLUTION FOR THE PROBLEM
 function getASCII(c){
     const asciiValue = c.charCodeAt(0);
   return asciiValue;
