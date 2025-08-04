37c539aa4dbce5829dc619c4af5cafeabaafe651
Dee <twitchg650@gmail.com>
2025-08-04 07:02:16 -0500

    Codewars

--- getEvenNumbers.js
+++ getEvenNumbers.js
@@ -6,7 +6,7 @@ The solution would work like the following:
 getEvenNumbers([2,4,5,6]) // should == [2,4,6]
  */
 
-//SOLUTION FOR THE PROBLEM
+//SOLUTION FOR THE PROBLEM!
 
 function getEvenNumbers(numbersArray) {
     return numbersArray.filter(num => num % 2 === 0);
