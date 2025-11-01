3a5e76b21fceacb4cff74e71458afb74e596dee8
Deija <m>
2025-11-01 07:42:59 -0500

    Codewars Daily

--- invert.js
+++ invert.js
@@ -6,7 +6,7 @@ invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
 invert([]) == []
 You can assume that all values are integers. Do not mutate the input array/list. */
 
-//SOLUTION
+//SOLUTION FOR THE PROBLEM!
 function invert(array) {
     return array.map(function(number) {
       return -number;
