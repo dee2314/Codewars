2992c32ed46304393e519538e13c328c51771d37
Deija <twitchg650@gmail.com>
2025-12-06 07:07:09 -0600

    Codewars daily

--- doubleChar.js
+++ doubleChar.js
@@ -6,7 +6,7 @@ Examples (Input -> Output):
 * "1234!_ "     -> "11223344!!__  "*/
 
 
-//SOLUTION FOR THE PROBLEM! 
+//SOLUTION FOR THE PROBLEM
 function doubleChar(str) {
     // Your code here
     let result = '';
